b32cfaa3ccc56c55f4a4567ce43df2a710e93304
Igor Šula <sulaigor@gmail.com>
2021-05-31 21:53:36 +0200

    Add console logs to script index file

--- index.ts
+++ index.ts
@@ -3,9 +3,18 @@ import { createTempFolder, createOutputFolder, removeTempFolder } from 'folderUt
 import { createComicPdfs } from 'createComicPdfs';
 
 (async () => {
+  console.log(
+    '\nI have',
+    comics.length,
+    'commics for download:',
+    comics.reduce((acc, { name }) => acc + name + '\n', '\n'),
+  );
+
   createOutputFolder();
   createTempFolder();
   await createComicPdfs(comics);
   removeTempFolder();
+
+  console.log('I am done. \nHave a nice reading!\n');
   process.exit(0);
 })();
