df7ea0d82a42b81ec80cd609773f8ca4133b450c
Igor Šula <sulaigor@gmail.com>
2022-01-16 13:03:51 +0100

    Edit how much script running

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
 import comics from 'comics';
 import { createTempFolder, createOutputFolder, removeTempFolder } from 'folderUtils';
 import { createComicPdfs } from 'createComicPdfs';
-import { printDownloadComics } from 'printDownloadComics';
 
 (async () => {
-  printDownloadComics(comics);
-
   createOutputFolder();
   createTempFolder();
   await createComicPdfs(comics);
