23082d1d65889af25df6a98de84297638a2381f5
Igor Šula <sulaigor@gmail.com>
2022-03-11 23:43:24 +0100

    Refactor comic model

--- src/comic.ts
+++ src/comic.ts
@@ -1,14 +1,14 @@
+import { COMICS_URL_PREFIX } from 'const';
+
 export interface IComic {
   name: string;
   url: string;
 }
 
 export class Comic implements IComic {
-  name: string;
   url: string;
 
-  constructor(name: string, path: string) {
-    this.name = name;
-    this.url = 'https://readcomiconline.to/Comic' + path;
+  constructor(public name: string, path: string) {
+    this.url = COMICS_URL_PREFIX + path;
   }
 }
--- src/const.ts
+++ src/const.ts
@@ -6,3 +6,5 @@ export const PDF_FOLDER_PATH = path.resolve(ROOT_PATH, 'pdf');
 
 export const URL_REGEX =
   /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
+
+export const COMICS_URL_PREFIX = 'https://readcomiconline.to/Comic';
