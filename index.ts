d3f0f1fe1e926d7d40af65e3df61f40b2e7426c8
Igor Šula <sulaigor@gmail.com>
2022-03-13 23:42:43 +0100

    Refactor scrap script

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import comics from 'comics';
 import { createTempFolder, createOutputFolder, removeTempFolder } from 'folderUtils';
 import { createComicPdfs } from 'createComicPdfs';
+import { exitScript } from 'consoleUtils';
 
 (async () => {
   createOutputFolder();
   createTempFolder();
   await createComicPdfs(comics);
   removeTempFolder();
-
-  console.log('\nI am done... \nHave a nice reading!\n');
+  exitScript();
 })();
