56e5f0f137d3151bd7ed66bd3105bef46a73cbc8
Igor Šula <sulaigor@gmail.com>
2022-03-13 23:48:55 +0100

    Refactor console utils

--- src/consoleUtils.ts
+++ src/consoleUtils.ts
@@ -2,8 +2,7 @@ import { ConsoleColours } from 'const';
 
 export const printColoredMessage = (color: ConsoleColours, msg: string): void => {
   console.log(color);
-  console.log(msg);
-  console.log(ConsoleColours.RESET);
+  console.log(msg, ConsoleColours.RESET);
 };
 
 export const exitScriptWithMessage = (color: ConsoleColours, msg: string): never => {
@@ -12,7 +11,7 @@ export const exitScriptWithMessage = (color: ConsoleColours, msg: string): never
 };
 
 export const exitScript = (): never =>
-  exitScriptWithMessage(ConsoleColours.GREEN, '\nI am done... \nHave a nice day!');
+  exitScriptWithMessage(ConsoleColours.GREEN, 'I am done... \nHave a nice day!\n');
 
 export const tryValue = (tryValue: any, msg: string): void => {
   if (!tryValue) {
