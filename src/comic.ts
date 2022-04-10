6d17f198693eeaae9dd37869c62ba4d06a5ca8e9
Igor Šula <sulaigor@gmail.com>
2022-04-10 23:42:45 +0200

    Add dto inicialization to comic model

--- src/comic.ts
+++ src/comic.ts
@@ -5,10 +5,32 @@ export interface IComic {
   url: string;
 }
 
-export class Comic implements IComic {
-  url: string;
+type ComicClassParams = [IComic] | [string, string];
 
-  constructor(public name: string, path: string) {
-    this.url = COMICS_URL_PREFIX + path;
+const getComicUrl = (urlOrPath: string): string =>
+  !urlOrPath.includes(COMICS_URL_PREFIX) ? COMICS_URL_PREFIX + urlOrPath : urlOrPath;
+
+export class Comic implements IComic {
+  public readonly name: string;
+  public readonly url: string;
+
+  constructor(comicDto: IComic);
+  constructor(name: string, urlOrPath: string);
+  constructor(...params: ComicClassParams) {
+    if (params.length === 2) {
+      const [name, urlOrPath] = params;
+      this.name = name;
+      this.url = getComicUrl(urlOrPath);
+      return;
+    }
+
+    if (params.length === 1 && typeof params[0] === 'object') {
+      const { name, url } = params[0];
+      this.name = name;
+      this.url = getComicUrl(url);
+      return;
+    }
+
+    throw new Error('Comic has invalid params!');
   }
 }
