1a1bc2041cb498b900e57776d4bdeae87b4bb55c
Igor Šula <sulaigor@gmail.com>
2022-04-30 00:54:32 +0200

    Refactor html utils with puppeteer

--- src/htmlUtils.ts
+++ src/htmlUtils.ts
@@ -1,13 +1,40 @@
-import axios from 'axios';
+import puppeteer from 'puppeteer-extra';
+import { FingerprintInjector } from 'fingerprint-injector';
 import { parse as parseHTML, HTMLElement } from 'node-html-parser';
 
-export const getHtml = async (htmlUrl: string): Promise<HTMLElement | null> => {
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const FingerprintGenerator = require('fingerprint-generator');
+
+const DEFAULT_WAIT_SELECTOR = '#containerRoot';
+
+export const getHtml = async (
+  htmlUrl: string,
+  waitForSelector: string = DEFAULT_WAIT_SELECTOR,
+): Promise<HTMLElement | null> => {
+  const fingerprintInjector = new FingerprintInjector();
+
+  const fingerprintGenerator = new FingerprintGenerator({
+    devices: ['desktop'],
+    browsers: [{ name: 'chrome', minVersion: 90 }],
+  });
+
+  const { fingerprint, headers } = fingerprintGenerator.getFingerprint();
+
   try {
-    const { data: html } = await axios.get<string>(htmlUrl);
-    return parseHTML(html);
+    const browser = await puppeteer.use(StealthPlugin()).launch({ headless: false });
+    const page = await browser.newPage();
+
+    await fingerprintInjector.attachFingerprintToPuppeteer(page, fingerprint);
+    await page.setJavaScriptEnabled(true);
+    await page.setExtraHTTPHeaders(headers);
+    await page.goto(htmlUrl);
+    await page.waitForSelector(waitForSelector, { timeout: 60000 });
+    const content = await page.content();
+    await browser.close();
+    return parseHTML(content);
   } catch (err) {
     const { message } = err as Error;
-    console.log('Get html error:', message);
+    console.log('Download html document failed:', message);
   }
 
   return null;
